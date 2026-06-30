0b80f1b4fd2f40f9b8a11d451ac06404c132cdec
alxire2 <a_wilcox@hotmail.com>
2026-06-29 21:19:30 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'ppl-lp-v1';
+const ASSETS = ['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
